adb45f08c4f9045b66174e173b8f82d90f16f3ad
ferstar <zhangjianfei3@gmail.com>
2016-02-26 14:17:56 +0800

    add comments api

--- run.py
+++ run.py
@@ -3,9 +3,8 @@ from datetime import datetime
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 from flask import Flask, request, make_response, jsonify
-from flask_restful import Api, Resource, abort
 from flask_pymongo import PyMongo
-
+from flask_restful import Api, Resource, abort
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
@@ -38,14 +37,22 @@ def check_content(obj):
     return obj
 
 
+class GeoPostList(Resource):
+    def get(self):  # get all posts
+        cursor = mongo.db.geo_posts.find({})
+        return check_content(cursor)
+
+    def post(self):  # add a new post
+        resp = request.get_json(force=True)
+        resp["_created"] = datetime.utcnow()
+        mongo.db.geo_posts.insert(resp)
+        return "", 201
+
+
 class GeoPost(Resource):
     def get(self, post_id):  # get a post by its ID
         cursor = mongo.db.geo_posts.find({"_id": ObjectId(post_id)})
         return check_content(cursor)
-
-    def delete(self, post_id):  # delete a post by its ID
-        cursor = mongo.db.geo_posts.remove({"_id": ObjectId(post_id)})
-        return "", 204
 
     def put(self, post_id):  # update a post by its ID
         resp = request.get_json(force=True)
@@ -57,25 +64,52 @@ class GeoPost(Resource):
         )
         return "", 204
 
+    def delete(self, post_id):  # delete a post by its ID
+        cursor = mongo.db.geo_posts.remove({"_id": ObjectId(post_id)})
+        # delete related comments
+        cursor = mongo.db.geo_comments.remove({"post_id": post_id})
+        return "", 204
 
-class GeoPostList(Resource):
-    def get(self):  # get all geo_posts
-        cursor = mongo.db.geo_posts.find({})
+
+class GeoCommentList(Resource):
+    def get(self):  # get all comments
+        cursor = mongo.db.geo_comments.find({})
         return check_content(cursor)
 
-    def post(self):  # add a new post
+    def post(self):  # add a new comment
         resp = request.get_json(force=True)
-        if "_created" in resp:
-            # turn an ISO 8601 string like: 2016-02-23T23:41:54.000Z into datetime object
-            resp["_created"] = datetime.utcnow()
-        mongo.db.geo_posts.insert(resp)
+        resp["_created"] = datetime.utcnow()
+        mongo.db.geo_comments.insert(resp)
         return "", 201
 
 
-api.add_resource(GeoPostList, '/geo_posts',
-                 '/geo_posts/', endpoint='geo_posts')
+class GeoComment(Resource):
+    def get(self, comment_id):  # get a comment by its ID
+        cursor = mongo.db.geo_comments.find({"_id": ObjectId(comment_id)})
+        return check_content(cursor)
+
+    def put(self, comment_id):  # update a comment by its ID
+        resp = request.get_json(force=True)
+        cursor = mongo.db.geo_comments.update_one(
+            {"_id": ObjectId(comment_id)},
+            {
+                "$set": resp
+            }
+        )
+        return "", 204
+
+    def delete(self, comment_id):  # delete a comment by its ID
+        cursor = mongo.db.geo_comments.remove({"_id": ObjectId(comment_id)})
+        return "", 204
+
+
+api.add_resource(GeoPostList, '/geo_posts', endpoint='geo_posts')
 api.add_resource(GeoPost, '/geo_posts/<string:post_id>',
-                 '/geo_posts/<string:post_id>/', endpoint='geo_post')
+                 endpoint='geo_post')
+api.add_resource(GeoCommentList, '/geo_comments', endpoint='geo_comments')
+api.add_resource(GeoComment, '/geo_comments/<string:comment_id>',
+                 endpoint='geo_comment')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
