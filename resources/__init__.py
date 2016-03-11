24c75bf0fe298795651aae3d86308adfe74c1e13
ferstar <zhangjianfei3@gmail.com>
2016-03-11 17:35:56 +0800

    optimize model import methods

---
+++ resources/__init__.py
@@ -0,0 +1,14 @@
+from resources.board_comment import BoardComments, BoardCommentsList
+from resources.board_post import BoardPosts, BoardPostsList
+from resources.comment import Comments, CommentsList, PostComments
+from resources.device import Devices, DevicesList
+from resources.device_trace import DeviceTrace, DeviceTraceList
+from resources.mask import Masks, MasksList
+from resources.message import Messages, MessagesList
+from resources.parameter import Parameters, ParametersList
+from resources.post import Posts, PostsList, FavorPost
+from resources.theme import Themes, ThemesList
+from resources.user import Users, UsersList, UserPostsList, UserCommentsList, \
+    UserStarsList
+from resources.user_level import UserLevels, UserLevelsList
+from resources.user_trace import UserTraces, UserTracesList
