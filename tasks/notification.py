5aa38cb38396c2e29d2b378994a3476a6d34a261
hugoxia <hugoxia88@163.com>
2016-04-08 18:32:35 +0800

    add some notifications

--- tasks/notification.py
+++ tasks/notification.py
@@ -8,30 +8,60 @@ log = logging.getLogger("masque.task.notifications")
 
 
 @app.task
-def new_reply(author_id, post_id, comment_id):
-    content = "your post %s have a new comment %s" % (post_id, comment_id)
+def new_reply(author_id, theme_id, post_id, comment_id):
+    content = "Your post %s have a new comment %s" % (post_id, comment_id)
     log.info(content)
     notifi = connection.Notifications()
     notifi.type = "message"
+    notifi.theme_id = theme_id
+    notifi.post_id = post_id
     notifi.user_id = author_id
     notifi.content = content
     notifi.save()
 
 
 @app.task
-def new_heart(author_id, post_id):
-    content = "your post %s have a new heart" % post_id
+def star_new_reply(author_id, theme_id, post_id, comment_id):
+    content = "There are new comments %s for the post %s you viewed" % (post_id, comment_id)
+    log.info(content)
+    notifi = connection.Notifications()
+    notifi.type = "message"
+    notifi.theme_id = theme_id
+    notifi.post_id = post_id
+    notifi.user_id = author_id
+    notifi.content = content
+    notifi.save()
+
+
+@app.task
+def comment_new_reply(author_id, theme_id, post_id, comment_id):
+    content = "There are new comments %s for the post %s you remarked" % (comment_id, post_id)
+    log.info(content)
+    notifi = connection.Notifications()
+    notifi.type = "message"
+    notifi.theme_id = theme_id
+    notifi.post_id = post_id
+    notifi.user_id = author_id
+    notifi.content = content
+    notifi.save()
+
+
+@app.task
+def new_heart(author_id, theme_id, post_id):
+    content = "Your post %s have a new heart" % post_id
     log.info(content)
     notifi = connection.Notifications()
     notifi.type = "message"
     notifi.user_id = author_id
+    notifi.theme_id = theme_id
+    notifi.post_id = post_id
     notifi.content = content
     notifi.save()
 
 
 @app.task
 def level_up(user_id, user_level):
-    content = "level up! your new level is %s" % user_level
+    content = "Level up! your new level is %s" % user_level
     log.info(content)
     notifi = connection.Notifications()
     notifi.type = "levelup"
@@ -42,7 +72,7 @@ def level_up(user_id, user_level):
 
 @app.task
 def level_down(user_id, user_level):
-    content = "level down! your new level is %s" % user_level
+    content = "Level down! your new level is %s" % user_level
     log.info(content)
     notifi = connection.Notifications()
     notifi.type = "punishment"
@@ -52,8 +82,8 @@ def level_down(user_id, user_level):
 
 
 @app.task
-def forbid_post(user_id, expiry=7*24*3600):
-    content = "punishment! you are prohibited to post %s" % expiry
+def forbid_post(user_id, expiry=7 * 24 * 3600):
+    content = "Warning! you are prohibited to post %s" % expiry
     log.info(content)
     notifi = connection.Notifications()
     notifi.type = "punishment"
@@ -64,7 +94,7 @@ def forbid_post(user_id, expiry=7*24*3600):
 
 @app.task
 def frozen_user(user_id):
-    content = "punishment! your account has been frozen "
+    content = "Warning! your account has been frozen "
     log.info(content)
     notifi = connection.Notifications()
     notifi.type = "punishment"
@@ -75,7 +105,7 @@ def frozen_user(user_id):
 
 @app.task
 def exp_up(user_id, user_exp):
-    content = "congratulation! your exp up to %s " % user_exp
+    content = "Congratulation! your exp up to %s " % user_exp
     log.info(content)
     notifi = connection.Notifications()
     notifi.type = "encourage"
@@ -86,7 +116,7 @@ def exp_up(user_id, user_exp):
 
 @app.task
 def update_system(user_id, version):
-    content = "great news, you have a new version %s can be updated" % version
+    content = "Great news, you have a new version %s can be updated" % version
     log.info(content)
     notifi = connection.Notifications()
     notifi.type = "message"
--- util.py
+++ util.py
@@ -30,12 +30,6 @@ def get_level(exp):
             return l['_id']
 
 
-def get_post_user(post_id):
-    """get all users in post_id"""
-    users = set(x['post_id'] for x in mongo['commens_%s' % post_id].find())
-    return users
-
-
 def add_exp(user, exp):
     """add exp to user 
     input:User instance"""
@@ -43,7 +37,7 @@ def add_exp(user, exp):
     new_level = get_level(user.exp)
     if new_level != user.user_level_id:
         user.user_level_id = new_level
-        notification.level_up.delay(user._id, user.user_level_id)
+        notification.level_up.delay(str(user._id), user.user_level_id)
 
 
 def minus_exp(user, exp):
@@ -53,35 +47,52 @@ def minus_exp(user, exp):
     new_level = get_level(user.exp)
     if new_level != user.user_level_id:
         user.user_level_id = new_level
-        notification.level_down.delay(user._id, user.user_level_id)
+        notification.level_down.delay(str(user._id), user.user_level_id)
 
 
-def new_comment(post):
+def new_remark(comment):
     """post have a new comment
-    input:Post instance"""
-    notification.new_reply.delay(post.author_id, post.post_id, post.comment_id)
+    input:Comment instance"""
+    user_stars = list(mongo.user_stars.find({'post_id': comment.post_id}))
+    user_comments = list(mongo.user_comments.find({'post_id': comment.post_id}))
+    notification.new_reply.delay(comment.author, comment.post_id, str(comment._id))
+    for star in user_stars:
+        notification.star_new_reply.deplay(star['user_id'], star['theme_id'],
+                                           star['post_id'], str(comment._id))
+    for element in user_comments:
+        notification.comment_new_reply.deplay(element['user_id'], element['theme_id'],
+                                              element['post_id'], str(comment._id))
 
 
-def self_post_heart(post):
+def post_heart(post):
     """post have a new heart
     input:Post instance"""
-    notification.new_heart.delay(post.author_id, post.post_id)
+    notification.new_heart.delay(post.author, str(post._id))
 
 
 def max_punish(user):
     """frozen user
     input:User instance"""
-    notification.frozen_user.delay(user._id)
+    notification.frozen_user.delay(str(user._id))
 
 
 def restrict_post(user, expire_time):
     """forbit user to post in expire_time, expire_time is in seconds
     input:User instance"""
-    notification.forbid_post.delay(user._id, expire_time)
+    notification.forbid_post.delay(str(user._id), expire_time)
 
 
 def update_system(user, version):
     """remind user to update software
     input:User instance"""
-    notification.forbid_post.delay(user._id, version)
+    notification.forbid_post.delay(str(user._id), version)
 
+
+class dict2obj(object):
+    """convert dict to object"""
+    def __init__(self, dic):
+        for k, v in dic.items():
+            if isinstance(v, (tuple, list)):
+                self.__setattr__(k, [dict2obj(i) if isinstance(i, dict) else i for i in v])
+            else:
+                self.__setattr__(k, dict2obj(v) if isinstance(v, dict) else v)
